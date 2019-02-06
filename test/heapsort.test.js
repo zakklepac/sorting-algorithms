6b5f53f62aae616e115b8e97daa550fbbb0335b5
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-02-06 13:48:00 -0600

    test commit

--- test/heapsort.test.js
+++ test/heapsort.test.js
@@ -1,1 +1,15 @@
 const expect = require('chai').expect
+const heapSort = require('../src/heapsort')
+
+describe('Heap Sort', () => {
+    let arr = [4,2,6,8,9,1]
+    let arr2 = [9,8,7,5,3,1]
+    let arr3 = [40,30,55,82,34,44]
+  
+    it('should return array sorted', () => {
+      expect(sort.heapSort(arr)).to.eql(arr.sort())
+      expect(sort.heapSort(arr2)).to.eql(arr2.sort())
+      expect(sort.heapSort(arr3)).to.eql(arr3.sort())
+  
+    })
+  })
--- test/quicksort.test.js
+++ test/quicksort.test.js
@@ -1,1 +1,17 @@
 const expect = require('chai').expect
+const quickSort = require('../src/quicksort')
+
+describe('Quick Sort', () => {
+    let arr1 = [4,2,6,8,9,1]
+    let arr2 = [1,4,7,9,2,8,3,6]
+    let arr3 = [40,30,55,82,34,44]
+  
+    it('should return array sorted', () => {
+      expect(sort.quickSort(arr1)).to.eql(arr1.sort())
+      expect(sort.quickSort(arr2)).to.eql(arr2.sort())
+      expect(sort.quickSort(arr3)).to.eql(arr3.sort())
+  
+    })
+  });
+
+  
